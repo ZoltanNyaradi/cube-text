d71a31230db9e9afc30d0875406207498bac007a
Zoltan Nyaradi <nyaradizoltan96@gmail.com>
2024-02-25 15:37:13 +0100

    Add function structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-//document.getElementsByTagName("h1")[1].innerHTML="Bye!";
+document.addEventListener("DOMContentLoaded",function(){
+	canvas = document.getElementsByTagName("canvas")[0];
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	let canvasWidth = 0;
+
+		
+	measure();
+
+	window.addEventListener("resize", measure);
+
+	document.getElementById("user-text").addEventListener("input",draw);
+	document.getElementById("color-comb").addEventListener("change",draw);
+	document.getElementById("cube-color").addEventListener("change",draw);
+
+	document.getElementById("download").addEventListener("click",download);
+	
+	document.getElementById("cube-background").addEventListener("change",changeBackground);
+})	
+function measure(){
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	canvasWidth = document.getElementsByTagName("canvas")[0].width;
+	
+	draw();
+}
+
+function draw(){
+	
+}
+
+function download(){
+	console.log("File downloaded!");
+}
+function changeBackground(){
+	console.log("Background changed!");
+}
